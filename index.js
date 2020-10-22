e5cd5c41b5bb5322ef9f7816030e3305d720b3ca
Your Name <you@example.com>
2020-10-22 17:39:50 +0000

    Initial commit

--- index.js
+++ index.js
@@ -3,8 +3,9 @@ require('dotenv').config()
 const bp = require("body-parser")
 let cookieParser = require('cookie-parser');
 const express = require('express');
-
+const path = require("path")
 const app = express();
+app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs')
 app.use(express.static("public"))
 app.use(cookieParser())
